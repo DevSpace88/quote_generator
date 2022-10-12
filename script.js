cf06aa9c64430a096ed428f844b7743b23be168a
DevSpace88 <sgorochow@hotmail.de>
2022-10-12 11:36:06 +0200

    added EventListeners for two Buttons

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 // Rest-API, let apiQuotes ist für Rest-API notwendig!
 // Ein leeres Array, damit wir später, den Wert ändern können. U.a. deswegen auch let, weil variabel
 let apiQuotes = [];
@@ -7,13 +14,29 @@ let apiQuotes = [];
 function newQuote() {
     // Pick a random Quote from API-Quotes-Array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+
+    // Check if Author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    //  Check Quote length to determine the styling
+    if (quoteText.length > 120) {
+        // classList has functions to add or remove css-styling
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
 
 //async function can run at anytime independently and won't stop the browser from completing loading the page
-async function getQuotes(){
+async function getQuotes() {
     // Rest-API
     const apiUrl = 'https://type.fit/api/quotes';
 
@@ -37,18 +60,18 @@ async function getQuotes(){
 }
 
 
+// Tweet Quote
+function tweetQuote() {
+    // Template-String, basically like f-Strings, uses backticks, not singlequotes
+    // ? will introduce a query-parameter, text= is the actual query-parameter, after that our variables
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    // opens twitterUrl, _blank means it will open in a new Tab
+    window.open(twitterUrl, '_blank');
+}
+
+// EventListeners, generally go at the bottom, no Brackets need for the second Parameter
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click',tweetQuote);
+
 // On Load 
 getQuotes();
-
-
-// // Quotes from local quotes.js
-
-// // To use this, <script src="quotes.js" defer></script> need to be in body of index.html
-// // Show new Quote Function
-// function newQuote() {
-//     // Pick a random Quote from API-Quotes-Array
-//     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-//     console.log(quote);
-// }
-
-// newQuote();
